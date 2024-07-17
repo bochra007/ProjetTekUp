a9dc00f6151c7b716b370402b01e53236e2339f1
bochra007 <bochrachaibi3@gmail.com>
2024-07-17 14:45:49 +0200

    ajout fichiers PHP

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+    list.forEach((item) => {
+        item.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
+}
+
+list.forEach(item => item.addEventListener("mouseover", activeLink));
+
+
+
+// Menu
+
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.addEventListener("click", function() {
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+});
